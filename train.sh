6b0a79c11f4d7b235fd176ef7d2c1861064d48df
hanzh0816 <15091693112@163.com>
2025-11-11 14:36:17 +0800

    修改train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,13 @@
 export NUPLAN_DATA_ROOT="/data2/hzh/nuplan/dataset"
 export NUPLAN_MAPS_ROOT="/data2/hzh/nuplan/dataset/maps"
-export NUPLAN_EXP_ROOT="/data2/hzh/nuplan/exp"
-export NUPLAN_DB_FILES="/data2/hzh/nuplan/dataset/nuplan-v1.1/splits/mini"
+export NUPLAN_EXP_ROOT="/home/hzh/code/planning/intent_planTF/work_dirs"
 
 python run_training.py \
     py_func=train \
     +training=train_planTF \
     cache.cache_path=/data2/hzh/nuplan/exp/cache_plantf_1M \
     cache.use_cache_without_dataset=true \
-    model.intent_enabled=true \
-    data_loader.params.batch_size=8 \
-    wandb.mode=online \
-    wandb.project=planTF \
-    wandb.name=intent_training
+    data_loader.params.batch_size=32 data_loader.params.num_workers=32 \
+    lr=1e-3 epochs=25 warmup_epochs=3 weight_decay=0.0001 \
+    lightning.trainer.params.val_check_interval=1.0 \
+    wandb.mode=online wandb.project=nuplan wandb.name=intent_plantf
